996f7d7c241af6878a91e4d7515bf1f24af2faf3
Rupali <kalerupali8@gmail.com>
2022-06-24 15:32:31 +0530

    First changes....

---
+++ src/gitdemo2/gitdemo2_class.java
@@ -0,0 +1,20 @@
+package gitdemo2;
+
+public class gitdemo2_class 
+{
+	 public static void main(String[] args) {
+			
+
+			String s1="my name is rupali";
+			
+			String [] ar = s1.split(" ");         //{my(0)    name(1)    is(2)    rahul(3)}
+			
+			  //     3
+			for(int i=ar.length-1; i>=0; i--) 
+			{
+				System.out.print(ar[i]+" ");      //
+			}
+
+		}
+
+}
